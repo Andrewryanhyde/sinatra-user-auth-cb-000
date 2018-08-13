c91fe46129d16e58dc091e1ce84228855f148b2c
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-13 08:57:59 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,4 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
-  def change 
-    create_table do |t|
-      t.string :name 
-      t.string :email 
-      t.string :password
-    end
-  end
-
 end
